b2f8cb4c270694429abb320f69112b1045b82632
Thiago Mazo <thiago.mazo@hotmail.com>
2020-05-22 18:55:08 -0300

    principal

---
+++ TabuadaTeste/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TabuadaTeste
+{
+    
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            int result = 0;
+            int num = 1;
+            int multi = 1;
+         
+            while(num < 9)
+            {
+                num += 1;
+
+                while( multi <= 10)
+                {
+                   
+                    result = num * multi;
+                   
+                    Console.WriteLine(num + "x" + multi + "=" + result);
+                   
+                    multi += 1;
+                }
+
+                multi = 1; 
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
